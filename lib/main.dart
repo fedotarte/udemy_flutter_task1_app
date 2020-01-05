040d2093c2e360ad49c6600ce623ae2bdef563d1
root <artyom.fedotov93@gmail.com>
2020-01-05 23:26:05 +0300

    expenses app draft

--- lib/main.dart
+++ lib/main.dart
@@ -1,112 +1,109 @@
 import 'package:flutter/material.dart';
-import './textapp.dart';
-import './textcontrol.dart';
+import 'package:intl/intl.dart';
+
+import 'models/transaction.dart';
 
 void main() => runApp(MyApp());
 
-
-//TODO add text, add button to change the text, and the appbar
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.teal,
-      ),
-//      home: MyHomePage(title: 'Flutter Demo Home Page'),
-        home: MyHomePage(title: "Task first")
+      title: 'Flutter App',
+      home: MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  String keyToText = "initial";
-  int g_counter = 0;
-
-  void _textChanger() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      g_counter++;
-      keyToText=="initial" ? keyToText = "reflection" : keyToText = "initial";
-    });
-  }
+class MyHomePage extends StatelessWidget {
+  final List<Transaction> transactions = [
+    Transaction(
+        id: "t1", title: "New Shoes", amount: 99.99, date: DateTime.now()),
+    Transaction(
+        id: "t2", title: "Gasoline", amount: 54.99, date: DateTime.now()),
+//    Transaction(
+//        id: "t3", title: "House expenses", amount: 53.99, date: DateTime.now())
+  ];
+  final titleController = TextEditingController();
+  final amountController = TextEditingController();
+//  String titleInput;
+//  String amountInput;
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('Flutter App'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Just push the button...',
+      body: Column(
+//        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Container(
+            width: double.infinity,
+            child: Card(
+              color: Colors.blue,
+              child: Text('CHART!'),
+              elevation: 5,
             ),
-            TextApp(g_counter, keyToText),
-          ],
-        ),
+          ),
+          Card(
+              elevation: 5,
+              child: Container(
+                  padding: EdgeInsets.all(10),
+                  child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: <Widget>[
+                        TextField(
+                          decoration: InputDecoration(labelText: 'Title'),
+                          controller: titleController,
+                        ),
+                        TextField(
+                          decoration: InputDecoration(labelText: 'Amount'),
+                          controller: amountController,
+                        ),
+                        FlatButton(
+                            child: Text('Add Transaction'),
+                            textColor: Colors.purple,
+                            onPressed: () {
+                              print(titleController.text);
+                            })
+                      ]))),
+          Column(
+              children: transactions.map((tx) {
+            return Card(
+                child: Row(
+              children: <Widget>[
+                Container(
+                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                  decoration: BoxDecoration(
+                      border: Border.all(color: Colors.purpleAccent, width: 2)),
+                  padding: EdgeInsets.all(10),
+                  child: Text('\$ ${tx.amount}',
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                          color: Colors.purple)),
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      tx.title,
+                      style:
+                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      DateFormat.yMMMd().format(tx.date),
+                      style: TextStyle(color: Colors.blueGrey),
+                    )
+                  ],
+                )
+              ],
+            ));
+          }).toList())
+        ],
       ),
-      floatingActionButton: TextController(_textChanger), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
